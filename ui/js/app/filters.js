dc354396286713d26e8f80f04787112b03461dd3
BigMacHomeSlice <geoffrey.colburn@demandmedia.com>
2014-08-14 18:13:22 -0500

    fix skills display

--- ui/js/app/filters.js
+++ ui/js/app/filters.js
@@ -14,3 +14,28 @@ hackFilters.filter('joinTags', function () {
         return (input || []).join(delimiter || ',');
     };
 });
+
+hackFilters.filter('joinSkills', function () {
+    var map = {
+        illustrator: 'Illustrator',
+        webdesigner: 'Web-Designer',
+        editor: 'Editor',
+        videographer: 'Videographer',
+        photographer: 'Photographer',
+        writer: 'Writer'
+    }
+    return function (input, delimiter) {
+        if (input === undefined) {
+            return '';
+        }
+        console.dir(input);
+        var tokens = [];
+        $.each(input, function(k,v) {
+            if (v === true) {
+                tokens.push(map[k]);
+            }
+        });
+
+        return (tokens || []).join(delimiter || ',');
+    };
+});
